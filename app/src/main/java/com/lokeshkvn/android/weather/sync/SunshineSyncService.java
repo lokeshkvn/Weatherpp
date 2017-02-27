6ed9ff878118a893e3d32801c1e2c2b89862d90f
LOKESH NANDANWAR <lokeshkvn321998@gmail.com>
2017-02-27 17:53:22 +0530

    Initial commit

---
+++ app/src/main/java/com/lokeshkvn/android/weather/sync/SunshineSyncService.java
@@ -0,0 +1,30 @@
+package com.lokeshkvn.android.weather.sync;
+
+/**
+ * Created by DELL PC on 27-02-2017.
+ */
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class SunshineSyncService extends Service {
+    private static final Object sSyncAdapterLock = new Object();
+    private static SunshineSyncAdapter sSunshineSyncAdapter = null;
+
+    @Override
+    public void onCreate() {
+        Log.d("SunshineSyncService", "onCreate - SunshineSyncService");
+        synchronized (sSyncAdapterLock) {
+            if (sSunshineSyncAdapter == null) {
+                sSunshineSyncAdapter = new SunshineSyncAdapter(getApplicationContext(), true);
+            }
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return sSunshineSyncAdapter.getSyncAdapterBinder();
+    }
+}
